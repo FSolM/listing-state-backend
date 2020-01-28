370489121e75af85819e60263542acf8882e9438
FSolM <fersolmarin@gmail.com>
2020-01-27 23:11:13 -0600

    users controller #create Modified

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
   def create
+    p User.new(user_params)
+    if User.create(user_params)
+      p params
+      render json: 'Success'
+    else
+      render json: 'Something went wrong'
+    end
+  end
+
+  def destroy
+  end
+
+  private def user_params
+    params.require(:user).permit(:user, :password)
   end
 end
