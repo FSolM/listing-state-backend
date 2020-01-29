0c62f6b5ff4df9cea79ab42a71fb4b4ee49984bd
FSolM <fersolmarin@gmail.com>
2020-01-29 16:34:17 -0600

    #create Added

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -6,8 +6,17 @@ class PropertiesController < ApplicationController
   end
 
   def create
+    if Property.create(property_params)
+      render json: { status: 'Success', code: 101, message: 'Property created' }
+    else
+      render json: { status: 'Error', code: 3100, message: 'Error during the property creation' }
+    end
   end
 
   def destroy
+  end
+
+  private def property_params
+    params.require(:property).permit(:name, :price, :description, :location, :type, :bedrooms, :bathrooms, :size, :owner)
   end
 end
