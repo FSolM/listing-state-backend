1b9d19c0a9cd2991ee2c5ed771aade52bab977b6
FSolM <fersolmarin@gmail.com>
2020-01-29 16:25:32 -0600

    routes Modified

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   scope '/api' do
     post 'user/signup', to: 'users#create'
     post 'user/login', to: 'users#search'
-    get 'user/delete/:id', to: 'users#destroy'
-
-    get 'session/create', to: 'sessions#create'
-    get 'session/delete', to: 'sessions#destroy'
+    delete 'user/delete/:id', to: 'users#destroy'
 
     get 'properties', to: 'properties#index'
     get 'property/:id', to: 'properties#show'
-    get 'property/create', to: 'properties#create'
-    get 'property/delete', to: 'properties#destroy'
+    post 'property/create', to: 'properties#create'
+    delete 'property/delete', to: 'properties#destroy'
   end
 end
