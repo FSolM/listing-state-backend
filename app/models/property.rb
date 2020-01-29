4fdfbeb85e38b90d06068eb80089cb759da012be
FSolM <fersolmarin@gmail.com>
2020-01-29 16:34:03 -0600

    Property Validations Added

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,10 @@
 class Property < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :location, presence: true
+  validates :type, presence: true
+  validates :bedrooms, presence: true
+  validates :bathrooms, presence: true
+  validates :size, presence: true
+  validates :owner, presence: true
 end
