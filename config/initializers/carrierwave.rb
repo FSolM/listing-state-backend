b3693de33cdd69ab4d8421361686121c9038337d
Carlos Sol <fersolmarin@hotmail.com>
2020-02-16 17:58:18 -0600

    CarrierWave's fog Configured

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,9 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: '',
+    aws_secret_access_key: '',
+  }
+  config.fog_directory = 'listing-state-imgstorage'
+  config.fog_public = false
+end
