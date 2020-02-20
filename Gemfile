fd0d091b21c789423e6b878f5fea85501353330f
FSolM <fersolmarin@gmail.com>
2020-02-20 02:15:03 -0600

    RSpec gem Installed

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'fog-aws'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 4.0.0.beta'
 end
 
 # Database Config
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_04_014545) do
-
-  create_table "favourites", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2020_01_27_192721) do
 
   create_table "properties", force: :cascade do |t|
     t.string "name"
